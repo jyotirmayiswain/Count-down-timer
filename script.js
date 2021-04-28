f43febc38ec2f0ad26cb4b28dd863e5940c13475
jyotirmayiswain <62475182+jyotirmayiswain@users.noreply.github.com>
2021-04-28 21:16:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const daysE1 = document.getElementById('days');
+const hoursE1 = document.getElementById('hours');
+const minsE1 = document.getElementById('mins');
+const secondsE1 = document.getElementById('seconds');
+
+
+const newyears='1 Jan 2022';
+function countdown(){
+    const newyeardate = new Date(newyears);
+    const currentdate = new Date();
+
+    const totalseconds = (newyeardate - currentdate) / 1000;
+
+    const days = Math.floor(totalseconds / 3600 / 24);
+
+    const hours = Math.floor(totalseconds / 3600) % 24;
+
+    const mins = Math.floor(totalseconds / 60) % 60;
+    
+    const seconds = Math.floor(totalseconds) % 60;
+
+
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = formattime(hours);
+    minsE1.innerHTML = formattime(mins);
+    secondsE1.innerHTML = formattime(seconds);
+}
+function formattime(time){
+    return time<10 ? (`0${time}`) : time;
+}
+countdown();
+
+setInterval(countdown,1000 )
